a358c95d0fadf955dc2a2ab1585e9c83e674e2ab
Vijay-Ky <vijayky007@gmail.com>
2022-11-23 17:03:27 +0530

    lang_fund

---
+++ Resources/java-if-else-English/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n % 2 == 0 && n == 2 && n <= 5)
+		{
+			System.out.println("Not Weird");
+		}
+		else if(n % 2 == 0 && n == 6 && n <= 20)
+		{
+			System.out.println("Weird");
+		}
+		else if(n % 2 == 0 && n > 20)
+		{
+			System.out.println("Not Weird");
+		}
+		else
+		{
+			System.out.println("Weird");
+		}
+	}
+}
