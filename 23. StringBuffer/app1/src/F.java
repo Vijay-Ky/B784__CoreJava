86da4bd58f92f16988a875a13f0e859fc51db254
Vijay-Ky <vijayky007@gmail.com>
2023-01-07 18:14:02 +0530

    stringcomplete2

--- 23. StringBuffer/app1/src/F.java
+++ 23. StringBuffer/app1/src/F.java
@@ -3,11 +3,12 @@ class F
 	public static void main(String[] args) 
 	{
 		StringBuffer sb = new StringBuffer();
-		System.out.println(sb.length());
+		System.out.println(sb.length());//0
 		System.out.println(sb.capacity());//default capacity is 16
 		System.out.println("----------------------");
-		sb.append("121345678911234567");
+		sb.append("12345678911234567");
 		System.out.println(sb.length());
-		System.out.println(sb.capacity());
+		//(oldcapcity * 2) + 2
+		System.out.println(sb.capacity());//(16*2)= 32 + 2= 34
 	}
 }
